64878dc06de2e0e1c6ee8d3cbced306d825fdc18
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2023-03-21 03:05:48 +0100

    First commit OTP

---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let OTP = '';
+function generateOTP() {
+    var digits = '0123456789';
+    for (let i = 0; i < 6; i++) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    return OTP;
+}
+generateOTP();
+console.log(OTP);
