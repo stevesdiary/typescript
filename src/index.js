af9fd983b283467e268a2bc35a774d2d597f1ca3
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2023-03-30 17:10:25 +0100

    function, classes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 "use strict";
-let OTP = '';
-function generateOTP() {
-    var digits = '0123456789';
-    for (let i = 0; i < 6; i++) {
-        OTP += digits[Math.floor(Math.random() * 10)];
+// //Function overload
+// type Filter = {
+//    (array: number[], predicate: (item: number) => boolean): number[];
+//    (array: string[], predicate: (item: string) => boolean): string[];
+//    (array: object[], predicate: (item: object) => boolean): object[];
+// }
+// function filter(array: any[], predicate: Function ) {
+//    let result = [];
+//    for (let i = 0; i < array.length; i++) {
+//       let item = array[i];
+//       if (predicate(item)) {
+//          result.push(item);
+//       }
+//    }
+//    return result;
+// }
+// let numbers = [1,2,3,4,5,6,7,8,9,10,12]
+// function greaterThanSeven(item: number){
+//    return item >7
+// }
+// console.log(filter(numbers,greaterThanSeven));
+// let animals = ["cat", "dog", "goat", "lion", "horse", "rat"]
+// function filterCats(item: string){
+//    return item === "cat";
+// }
+// console.log(filter(animals, filterCats));
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        console.log("name: " + name);
+        console.log("email: " + email);
     }
-    return OTP;
+    greet() {
+        return `Hello ${this.name}`;
+    }
 }
-generateOTP();
-console.log(OTP);
+const person = new Person("John", "john@email.com");
+const person2 = new Person("Mark", "mark@email.com");
+console.log(person);
+console.log(person2);
+console.log(person.greet());
+console.log(person2.greet());
