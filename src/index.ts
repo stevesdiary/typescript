38a943d35979ecdd346b48931ce3f903c4b82bc0
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2023-04-04 13:42:48 +0100

    Protected members in a class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
-//Access modifiers in classes (public(can be used outside the class), protected(can be  accessed within the class and child class alone), private(can only be accessed within the parent class itself alone.))
+//Access modifiers in classes (
+   //public(can be used outside the class), 
+   //protected(can be  accessed within the class and child class alone), 
+   //private(can only be accessed within the parent class itself alone.)
+   //)
 class Person {
-   name: string;
-   age: number;
+   protected name: string;
+   public age: number;
 
    constructor(name: string, age: number) {
       this.name = name;
       this.age = age;
    }
+   public getName(){
+      return this.name;
+   }
 }
 
+class Admin extends Person {
+   public returnName(){
+      return this.name;
+   }
+}
+
+const admin: Admin = new Admin("Mark", 42);
+
 let person : Person = new Person("John", 32);
-person.name = "Mark"; //modifies the name of the person "John" to "Mark"
+//person.name = "Mark"; //modifies the name of the person "John" to "Mark"
 console.log(person);
+console.log(admin.returnName())
