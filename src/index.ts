00ef9e49edaad84da587d8bf82cb831197937c89
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2023-04-04 11:04:16 +0100

    Access modifiers in  cclasses

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,14 @@
-class User {
+//Access modifiers in classes (public(can be used outside the class), protected(can be  accessed within the class and child class alone), private(can only be accessed within the parent class itself alone.))
+class Person {
    name: string;
-   email: string;
    age: number;
 
-   constructor(name: string, email: string, age: number) {
+   constructor(name: string, age: number) {
       this.name = name;
-      this.email = email;
       this.age = age;
    }
 }
-//'extends' keyword is used to inheriit properties from a parent class
 
-class AdminUser extends User {
-   isAdmin: boolean = true;
-}
-
-const user: User = new User("John", "john@email.com", 23);
-const admin: AdminUser = new AdminUser("Danny", "dannyboy@email.com", 34)
-
-console.log(user)
-console.log(admin)
-
-//the result of the expression shows that the user properties are inherited from the user class 
-//and for the admin, there is an additional property 'isAdmin' which was included in the properties
+let person : Person = new Person("John", 32);
+person.name = "Mark"; //modifies the name of the person "John" to "Mark"
+console.log(person);
