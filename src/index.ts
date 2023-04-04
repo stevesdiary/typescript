6256d759a7a6f619548e617d2cce5d3d2b230da4
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2023-04-04 13:52:28 +0100

    Shorthand for initial properties

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,7 @@
    //private(can only be accessed within the parent class itself alone.)
    //)
 class Person {
-   protected name: string;
-   public age: number;
-
-   constructor(name: string, age: number) {
-      this.name = name;
-      this.age = age;
-   }
+   constructor(protected name: string, public age: number) {}
    public getName(){
       return this.name;
    }
