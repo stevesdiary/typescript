243bbdc8e12b12615495d7cea383704946a96ac5
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2023-04-14 12:33:46 +0100

    'this' keyword

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,14 @@
-//Protected Constructor and Child Classes
-type Holidays = {
-   date: Date;
-   reason: string;
-}[];
-
-abstract class Department {
-   protected abstract holidays: Holidays;
-   protected constructor(protected name: string){}
-
-   public addHolidays(holidays: Holidays){
-      if (Array.isArray(holidays)){
-         for(const holiday of holidays){
-            this.holidays.push(holiday);
-         }
-      }
-   }
-   public abstract printHolidays(): void;
-}
-//
-class ItDepartment extends Department {
-   protected holidays: Holidays = [];
-
-   constructor(){
-      super("IT Department");
-   }
-   public printHolidays() {
-      if (this.holidays.length === 0) {
-         return console.log("There are no holdays")
-   }
-   console.log(`Here's the list of holidays ${this.name}`);
-
-   this.holidays.forEach((holiday, index)=>{
-      console.log(`${index +1}. ${holiday.reason}, ${holiday.date}`
-      );
-   });
-}
-
-class AdminDepartment extends Department {
-   protected holidays: Holidays = [];
-
-   constructor () {
-      super("Admin Department");
-   }
-   public printHolidays(){
-      if (this.holidays.length === 0) {
-         return console.log("There are no holidays")
-      }
-      console.log(
-         `Here's the list of holidays ${this.name}`
-         );
-         this.holidays.forEach((holiday, index )=> {
-         console.log(`${index + 1}. ${holiday.reason}, ${holiday.date}`
-         );
-      });
+const book = {
+   title: "The Title",
+   authors: ["John", "Mark", "Rob"],
+   read(){
+      console.log(this);
+   },
+   
+   printAuthors(){
+      this.authors.forEach(function(author:string){
+         console.log(this.title, '-', author)
+      }, this)
    }
 }
-
-const itHolidays: Holidays =[
-   {
-      date: new Date(2022, 11, 1),
-      reason: "IT Department Day"
-   },
-   {
-      date: new Date(2022, 12, 25),
-      reason: "Christmas"
-   }
-];
-
-const adminHolidays: Holidays =[
-   {
-      date: new Date(2022, 10, 1),
-      reason: "Admin Department Day"
-   },
-   {
-      date: new Date(2022, 12, 25),
-      reason: "Christmas"
-   },
-];
-
-const itDepartment: ItDepartment = new ItDepartment();
-const adminDepartment: AdminDepartment = new AdminDepartment();
-
-itDepartment.addHolidays(itHolidays);
-adminDepartment.addHolidays(adminHolidays);
-
-itDepartment.printHolidays();
-adminDepartment.printHolidays();
+book.printAuthors()
