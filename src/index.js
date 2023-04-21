0046dcacb3a2ede23cfff6dd4a730649b922a4b7
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2023-04-21 13:43:08 +0100

    Using  this keyword

---
+++ src/index.js
@@ -0,0 +1,23 @@
+class User {
+   constructor(name, email){
+      this.name = name;
+      this.email = email;
+      this.points = 0;
+   }
+   login(){
+      console.log(this.name, 'has logged in');
+   }
+   logout(){
+      console.log(this.name, 'has logged out');
+   }
+   addPoint(){
+      this.points++;
+      console.log('total points', this.points);
+   }
+}
+
+const user = new User( 'John', 'john@email.com')
+const user2 = new User('Mark', 'mark@email.com')
+
+console.log(user);
+user2.addPoint();
